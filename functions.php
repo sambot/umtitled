0b32e8afd0470dd1a33761498a348d06b8ba0179
Sam <mail@sambot.com>
2012-03-17 09:36:03 -0400

    Reusable function to make post title linkable

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 
 
-// HELPERS //
+// HELPERS
 	
 	include_once ('config.php');
 	include_once ('markdown.php');
@@ -11,56 +11,50 @@
 			
 // FUNCTIONS
 
+
+	// Creates links to posts from titles and prints HTML post
+	
+	function title_post($filepaths,$i) { 
+		$single_post = preg_replace('#posts/[0-9-]*_([0-9a-z-]*)\.md#', '$1', $filepaths[$i]);
+		$md_content = file_get_contents($filepaths[$i]);
+		$html_content = (Markdown($md_content));
+		$html_content = str_replace('<h1>', '<h1><a href="'.$single_post.'">', $html_content);
+		$html_content = str_replace('</h1>', '</a></h1>', $html_content);
+		
+		echo($html_content);	
+	}
+	
+	
+
 	// Builds out the home page
 
-	function main_page() {
+	function main_page() { 
 	
 		$count = $GLOBALS['posts_per_page'] - 1;
-	
 		$filepaths = glob($GLOBALS['posts_dir'].'/*.md');
 		
 		for ($i=0; $i<=$count; $i++) {
-		
-			// START HERE //
-			$single_post = preg_replace('#posts/[0-9-]*_([a-z-]*)\.md#', '$1', $filepaths[$i]);
-		
-			$md_content = file_get_contents($filepaths[$i]);
-		
-			$html_content = (Markdown($md_content));
-			
-			$html_content = str_replace('<h1>', '<h1><a href="'.$single_post.'">', $html_content);
-			$html_content = str_replace('</h1>', '</h1></a>', $html_content);
-			
-			echo($html_content);
-		
+			title_post($filepaths,$i);
 		}
 		
 		echo('<a href="2">'.$GLOBALS['prev_page_text'].'</a>');
 	
 	}
-	
 
 	
+	
 	// Builds out the paginated posts pages
-		
-	function more_posts() {
 	
+	function more_posts() { 	
 		$page_num = $_SERVER['QUERY_STRING'];
-		
 		$ppp = $GLOBALS['posts_per_page'];
-	
 		$filepaths = glob($GLOBALS['posts_dir'].'/*.md');
-		
 		$end = $ppp * $page_num;
 		$start = $end - $ppp;
 		$total = count($filepaths);
 		
 		for ($i=$start; $i<=$end-1; $i++) {
-		
-			$post_content = file_get_contents($filepaths[$i]);
-		
-			echo (Markdown($post_content));
-		
+			title_post($filepaths,$i);
 		}
 		
 		if ($end < $total) {
