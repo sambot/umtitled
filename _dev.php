13da63f94317cbfe7c684b8e9a29efc8a194aafa
Sam <mail@sambot.com>
2012-03-21 15:54:29 -0400

    Public folder page scrape using Simple HTML DOM

--- _dev.php
+++ _dev.php
@@ -1,14 +1,31 @@
 <?php 
 
+include_once('_simplehtmldom/simple_html_dom.php');
 
-//$result = preg_replace('[uel*]', 'bot', 'samuel');
+$html = file_get_html('https://www.dropbox.com/sh/isyrm6akavgy8mh/obUwTUmZf5');
 
-
-$result = preg_replace('#posts/[0-9-]*_([a-z-]*)\.md#', '$1', 'posts/2012-03-16-23-07_an-awesome-post.md');
-
-echo 'result: ' . $result;
-
-
+?><!doctype html>
+<head>
+<title></title>
 
 
-?>
+</head>
+<body>
+<div id="result"></div>
+
+
+<?php 
+
+foreach($html->find('.filename-link') as $element) {
+	// echo $element->href . '<br>';
+	
+	$link = $element->href;
+	$link = preg_replace('#.*/#', '', $link);
+	echo ('<a href="http://dl.dropbox.com/u/1276566/umtitled/posts/'.$link.'">'.$link.'</a><br />');
+}
+
+ ?>
+
+
+</body>
+</html>
