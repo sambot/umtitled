d5026984d13213cf686bc856b48af7fec4922ff1
Sam <mail@sambot.com>
2012-03-27 13:29:01 -0400

    Removed pages from the main_page filepaths

--- functions/main_page.php
+++ functions/main_page.php
@@ -5,11 +5,27 @@
 	function main_page() { 
 		
 		filepaths();
+		
+		$filepaths = $GLOBALS['filepaths'];
+		
+		$loop_count = 0;
+		
+		
+		foreach ($filepaths as $file) { // cleans pages from posts
+			$this_file = str_replace($GLOBALS['dropbox_posts_dir'].'/', '', $file);
+			$this_file = substr($this_file, 0, 1);
+			if (ctype_alpha($this_file)) {
+				unset($filepaths[$loop_count]);
+			}
+			$loop_count++;
+		}
+		
+		$filepaths = array_values($filepaths); // resets array keys after page cleanse
 			
 		$count = $GLOBALS['posts_per_page'] - 1;
 		
 		for ($i=0; $i<=$count; $i++) {
-			title_post($GLOBALS['filepaths'],$i);
+			title_post($filepaths,$i);
 		}
 		
 		echo('<a href="2">'.$GLOBALS['prev_page_text'].'</a>');
