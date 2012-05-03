34251a0a424c97a643108c69415894787cb18edd
Sam <mail@sambot.com>
2012-05-03 08:45:16 -0400

    Dropbox changed things. So I changed things.
    (i hope this is not an indication of fragility)

--- scraped.php
+++ scraped.php
@@ -7,10 +7,24 @@ include_once ('cache/begin_caching_scrape.php');
 	
 	echo ('<!doctype html><html><head><title></title></head><body>');
 	
+	$links = array();
+	
 	foreach($html->find('.filename-link') as $element) {
 		$md = $element->href;
 		$md = preg_replace('#.*/#', '', $md);
-		echo('<a class="filename-link" href="'.$GLOBALS['dropbox_posts_dir'].'/'.$md.'">'.$GLOBALS['dropbox_posts_dir'].'/'.$md.'</a><br />');
+		
+		// echo('<a class="filename-link" href="'.$GLOBALS['dropbox_posts_dir'].'/'.$md.'">'.$GLOBALS['dropbox_posts_dir'].'/'.$md.'</a><br />');
+		
+		array_push($links,'<a class="filename-link" href="'.$GLOBALS['dropbox_posts_dir'].'/'.$md.'">'.$GLOBALS['dropbox_posts_dir'].'/'.$md.'</a><br />');
+		
+	}
+	
+	$links = array_unique($links);
+	
+	// print_r($links);
+	
+	foreach($links as $link) {
+		echo $link;
 	}
 	
 	echo ('</body></html>');
