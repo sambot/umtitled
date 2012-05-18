2e7feb614f512f51ff2e53339e9a76d9060e4965
Sam <mail@sambot.com>
2012-05-18 15:42:08 -0400

    Fixed post date... done with legacy PHP fixes... I think

--- functions/post_date.php
+++ functions/post_date.php
@@ -15,8 +15,17 @@
 		// $post_date = $post_date->format('F j, Y \a\t g:i a');
 		
 		// ADDED FOR PHP 5.2 COMPATIBILITY
+
+		$date_string = substr($post_date, 0, 10);
+
+		$time_string = substr($post_date, 12);
+		$time_string = str_replace('-', ':', $time_string);
+
+		$post_date = $date_string.' '.$time_string.':00';
+
 		$post_date = strtotime($post_date);
-		$post_date = strftime('%B %e, %Y at %l:%I %p', $post_date);
+
+		$post_date = strftime('%B %e, %Y at %l:%M %p', $post_date);
 	
 	}
 
