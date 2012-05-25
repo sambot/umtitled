895684442820dbffed0655795afe93992a6e0688
Sam <mail@sambot.com>
2012-05-25 13:39:48 -0400

    Small theme change for iphone width.

--- themes/dfawlt/top.php
+++ themes/dfawlt/top.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=640" /> 
 <title><?php echo ($site_title.' | '.$site_tagline); ?></title>
 <link rel="stylesheet" href="themes/<?php echo($theme); ?>/styles/reset.css">
 <link href='http://fonts.googleapis.com/css?family=Lato:300,700,900' rel='stylesheet'>
