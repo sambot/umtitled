0cef6b488f91d3764b87963adf4a5035d8f8e97a
Sam <mail@sambot.com>
2012-05-27 10:34:51 -0400

    Theme active nav link

--- functions/nav.php
+++ functions/nav.php
@@ -6,13 +6,21 @@
 		
 		filepaths();
 		remove_posts();
-		
+		current_url();
+
 		foreach ($GLOBALS['filepaths'] as $files) {
 			$page_link = preg_replace('#'.$GLOBALS['dropbox_posts_dir'].'/([a-z-]*)_[0-9]*\.md#', '$1', $files);
 			$the_file = fopen($files, 'r');
 			$page_title = fgets($the_file);
 			$page_title = str_replace('# ', '', $page_title);
-			echo ('<li><a href="'.$page_link.'">'.$page_title.'</a></li>');
+
+			if (strpos($GLOBALS['current_url'], $page_link)) {
+				$active_nav = ' class="active_nav"';
+			} else {
+				$active_nav = '';
+			}
+			
+			echo ('<li><a href="'.$page_link.'"'.$active_nav.'>'.$page_title.'</a></li>');
 		}
 	
 	}
